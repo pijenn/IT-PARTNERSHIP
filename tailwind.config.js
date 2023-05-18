ff5ebf691d02b05eff467dfee8f9ae872d6cf59d
Reza Bagus Saputra <103319101+RezaBagusS@users.noreply.github.com>
2023-05-18 11:59:19 +0700

    setup color

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,13 +10,9 @@ export default {
         poppins: ["Poppins", "sans-serif"],
       },
       colors: {
-        "cust-yellow": "#f9d816",
-        "cust-orange": "#ff6611",
-        "cust-blue": "#19397f",
-        "cust-purple": "#be0575",
-        "cust-beige": "#FDFAE2",
-        "cust-artikel": "#FB9C67",
-        "cust-budaya": "#FFB803",
+        "cust-blue": "#00123A",
+        "cust-sky": "#89A4DE",
+        "cust-light": "#F9F9F9"
       },
     },
   },
