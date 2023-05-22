7e1ad15729214f22bf02c704e1600da67d88a631
Reza Bagus Saputra <103319101+RezaBagusS@users.noreply.github.com>
2023-05-22 17:45:17 +0700

    set route

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from './App.jsx'
 import 'atropos/css'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/itpartnership" element={<App />} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
 )
