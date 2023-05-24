4d2eb4b2efa9fa3fc4df8552a53641cf96a1c3dd
kuhakuni <afif.haidar@student.ub.ac.id>
2023-05-24 17:42:31 +0700

    fix deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import App from './App.jsx'
-import 'atropos/css'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import App from "./App.jsx";
+import "atropos/css";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
-    </Router>
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<Router>
+		<Routes>
+			<Route path="/" element={<App />} />
+		</Routes>
+	</Router>
+);
