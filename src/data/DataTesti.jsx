4ef8de7f19854e1340b46e945183621bc3f617a0
Reza Bagus Saputra <103319101+RezaBagusS@users.noreply.github.com>
2023-05-29 13:20:20 +0700

    add data testi

--- src/data/DataTesti.jsx
+++ src/data/DataTesti.jsx
@@ -1,22 +1,34 @@
 const DataTesti = [
-    {
-        id: 1,
-        msg: "Gaada kritik, udah keren banget makasih banyak PIT bantuannya di oprec staff ahli 2023!!!",
-        name: "BEM FILKOM UB 2023",
-        logo: "https://res.cloudinary.com/dr0lbokc5/image/upload/v1684387560/Logo_BEM_nxphj5.svg",
-    },
-    {
-        id: 2,
-        msg: "Tidak ada kritik maupun saran, sudah baik dan sangat membantu. Terima kasih dan semangat terus untuk teman-teman PIT ✊🏻",
-        name: "SCHOTIVAL 2023",
-        logo: "https://res.cloudinary.com/dr0lbokc5/image/upload/v1684387560/Logo_BEM_nxphj5.svg",
-    },
-    {
-        id: 3,
-        msg: "semangat terus buat temen2 pit kalian keren 🙌🏻",
-        name: "PKKMB, HOLOGY, ARTROPOLIS, FILAFEST 2023",
-        logo: "https://res.cloudinary.com/dr0lbokc5/image/upload/v1684387560/Logo_BEM_nxphj5.svg",
-    },
+  {
+    id: 1,
+    msg: "Gaada kritik, udah keren banget makasih banyak PIT bantuannya di oprec staff ahli 2023!!!",
+    name: "BEM FILKOM UB 2023",
+    logo: "https://res.cloudinary.com/dr0lbokc5/image/upload/v1684387560/Logo_BEM_nxphj5.svg",
+  },
+  {
+    id: 2,
+    msg: "Tidak ada kritik maupun saran, sudah baik dan sangat membantu. Terima kasih dan semangat terus untuk teman-teman PIT ✊🏻",
+    name: "SCHOTIVAL 2023",
+    logo: "https://res.cloudinary.com/dr0lbokc5/image/upload/v1684387560/Logo_BEM_nxphj5.svg",
+  },
+  {
+    id: 3,
+    msg: "semangat terus buat temen2 pit kalian keren 🙌🏻",
+    name: "PKKMB, HOLOGY, ARTROPOLIS, FILAFEST 2023",
+    logo: "https://res.cloudinary.com/dr0lbokc5/image/upload/v1684387560/Logo_BEM_nxphj5.svg",
+  },
+  {
+    id: 4,
+    msg: "Website BEM Filkom Nawasena menggunakan warna yang cerah dan harmonis membuatnya terlihat menarik. Saya sangat mengapresiasi perbaikan yang dilakukan dari versi sebelumnya, terutama penambahan fitur filter pada halaman kabar filkom yang memudahkan pengguna untuk mencari informasi yang mereka butuhkan. Dengan segala perbaikan tersebut, saya memberikan rating 9/10 untuk website ini.",
+    name: "Farrel Reginaldo FILKOM 21",
+    logo: "",
+  },
+  {
+    id: 5,
+    msg: "Websitenya udah keren bangettt, informatif sekaliii, navigasinya mudah, komposisi warnanya juga udah pas banget dan enak diliatnyaaa, PIT Kaliann kerennn sekali !! semangatt terus",
+    name: "Noor Leona Asiyah FILKOM 21",
+    logo: "",
+  }
 ];
 
 export default DataTesti;
